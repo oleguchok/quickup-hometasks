44614fc7b7a900bf8a117c6ef8b80d47048bb3b6
oleguchok <oleguchok@gmail.com>
2016-11-03 01:00:55 +0300

    Add codefirst

---
+++ QuickUp.HomeTasks/CodeFirst.ConsoleApp/BloggingContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirst.ConsoleApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.ConsoleApp
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CodeFirst;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ QuickUp.HomeTasks/CodeFirst.ConsoleApp/Migrations/20161102215425_MyFirstMigration.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using CodeFirst.ConsoleApp;
+
+namespace CodeFirst.ConsoleApp.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20161102215425_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("CodeFirst.ConsoleApp.Model.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("CodeFirst.ConsoleApp.Model.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("CodeFirst.ConsoleApp.Model.Post", b =>
+                {
+                    b.HasOne("CodeFirst.ConsoleApp.Model.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
